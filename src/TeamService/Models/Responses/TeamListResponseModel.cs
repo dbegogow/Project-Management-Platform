29d534832177bc6250173cd89026f249f45f1ae5
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-05-25 23:31:48 +0300

    Create TeamListResponseModel in the Team service

---
+++ src/TeamService/Models/Responses/TeamListResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TeamService.Models.Responses;
+
+public class TeamListResponseModel
+{
+    public string Name { get; init; }
+
+    public string Goals { get; init; }
+
+    public IEnumerable<string> MembersUsernames { get; init; }
+}
